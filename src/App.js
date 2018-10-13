6147cd2b5b4fc3af1266a453c673d04d28c16a1e
lwkchan <laurachan205@gmail.com>
2018-10-13 17:48:07 +0100

    feat: add simple sign up page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      isLoggedIn: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.getForm = this.getForm.bind(this)
+    this.getAccountDashboard = this.getAccountDashboard.bind(this)
+  }
+
+  handleSubmit () {
+    this.setState({isLoggedIn: true})
+  }
+
+  getAccountDashboard () {
+    return (
+      <div data-testid='account-dashboard' >
+        <p>Hello {this.state.email}</p>
+      </div>
+    )
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+
+  getForm () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Welcome to flatfair
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <label> E-mail
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleInputChange} />
+          </label>
+          <label> Password
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleInputChange} />
+          </label>
+          <label> Re-enter Password
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Re-enter Password"
+              value={this.state.confirmPassword}
+              onChange={this.handleInputChange} />
+          </label>
+          <button name="Submit" type="submit" value="Submit">Submit</button>
+        </form>
       </div>
     );
+  }
+
+  render() {
+    return (
+      this.state.isLoggedIn
+      ? this.getAccountDashboard()
+      : this.getForm()
+    )
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'
+import React from 'react'
+import { render, fireEvent, waitForElement } from 'react-testing-library'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+    describe('When I first visit the homepage', () => {
+        it('Shows me what site I am on', () => {
+            const { getByText } = render(<App/>),
+            welcomeText = getByText('Welcome to flatfair')
+
+            expect(welcomeText).toBeDefined()
+        })
+        it('Has a form I can fill in for a new account', async () => {
+            const { getByLabelText, getByText, getByTestId } = render(<App/>),
+            emailInput = getByLabelText('E-mail'),
+            passwordInput = getByLabelText('Password'),
+            secondPasswordInput = getByLabelText('Re-enter Password')
+
+            fireEvent.change(emailInput, {
+                target: { value: 'test@email.com' }
+            })
+            fireEvent.change(passwordInput, {
+                target: { value: 'password123' }
+            })
+            fireEvent.change(secondPasswordInput, {
+                target: { value: 'password123' }
+            })
+
+            fireEvent.click(getByText('Submit'))
+
+            const accountDashboard = await waitForElement(() => getByTestId('account-dashboard'))
+
+            expect(accountDashboard.innerHTML).toContain('test@email.com')
+        })
+    })
+})
