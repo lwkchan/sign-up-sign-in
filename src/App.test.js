cb8b77f40958b99d44088fba0fa15d32e4fd8c61
lwkchan <laurachan205@gmail.com>
2018-10-14 23:26:23 +0100

    test: update unit tests to compensate for new routing of app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import App from './App'
 import React from 'react'
 import { render, fireEvent, waitForElement } from 'react-testing-library'
+import firebase from 'firebase'
 
 describe('App', () => {
+    let firebaseSpy
+    beforeEach(() => {
+        // Mock connection to firebase
+        firebaseSpy = jest.spyOn(firebase, 'initializeApp').mockReturnValue(true)
+    })
+    it('initializes firebase when rendered', () => {
+        render(<App/>)
+        expect(firebaseSpy).toHaveBeenCalledTimes(1)
+    })
     describe('When I first visit the homepage', () => {
         it('Shows me what site I am on', () => {
             const { getByText } = render(<App/>),
@@ -10,27 +20,32 @@ describe('App', () => {
 
             expect(welcomeText).toBeDefined()
         })
-        it('Has a form I can fill in for a new account', async () => {
-            const { getByLabelText, getByText, getByTestId } = render(<App/>),
-            emailInput = getByLabelText('E-mail'),
-            passwordInput = getByLabelText('Password'),
-            secondPasswordInput = getByLabelText('Re-enter Password')
+        it('I can fill in a form to sign up', async () => {
+            // Mock call to firebase
+            const mockAuthInterface = {
+                createUserWithEmailAndPassword: () => Promise.resolve()
+            },
+            signUpSpy = jest.spyOn(mockAuthInterface, 'createUserWithEmailAndPassword')
+            jest.spyOn(firebase, 'auth').mockReturnValue(mockAuthInterface)
 
-            fireEvent.change(emailInput, {
+            const { getByLabelText, getByText, getByTestId } = render(<App/>)
+
+            fireEvent.click(getByText('Go to sign up.'))
+            await waitForElement(() => getByLabelText('Email'))
+            fireEvent.change(getByLabelText('Email'), {
                 target: { value: 'test@email.com' }
             })
-            fireEvent.change(passwordInput, {
+            fireEvent.change(getByLabelText('Password'), {
                 target: { value: 'password123' }
             })
-            fireEvent.change(secondPasswordInput, {
+            fireEvent.change(getByLabelText('Re-enter Password'), {
                 target: { value: 'password123' }
             })
 
             fireEvent.click(getByText('Submit'))
+            await waitForElement(() => getByTestId('account-dashboard'))
 
-            const accountDashboard = await waitForElement(() => getByTestId('account-dashboard'))
-
-            expect(accountDashboard.innerHTML).toContain('test@email.com')
+            expect(signUpSpy).toHaveBeenCalledTimes(1)
         })
     })
 })
--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -5,32 +5,36 @@ import { render, cleanup } from 'react-testing-library'
 afterEach(cleanup)
 
 describe('Form', () => {
+    let errorProp
+    beforeEach(() => {
+        errorProp = { message: 'error' }
+    })
     it('Has a email input', () => {
-        const { queryAllByText } = render(<Form/>),
-        emailInput = queryAllByText('E-mail')
+        const { queryAllByText } = render(<Form error={errorProp}/>),
+        emailInput = queryAllByText('Email')
 
         expect(emailInput).toHaveLength(1)
     })
     it('Has one password input', () => {
-        const { queryAllByText } = render(<Form/>),
+        const { queryAllByText } = render(<Form error={errorProp}/>),
         passwordInput = queryAllByText('Password')
 
         expect(passwordInput).toHaveLength(1)
     })
     it('Has no confirm password input', () => {
-        const { queryAllByText } = render(<Form/>),
+        const { queryAllByText } = render(<Form error={errorProp}/>),
         confirmPasswordInput = queryAllByText('Re-enter Password')
 
         expect(confirmPasswordInput).toHaveLength(0)
     })
     it('Has one confirm password input when the signUp prop is true', () => {
-        const { queryAllByText } = render(<Form signUp/>),
+        const { queryAllByText } = render(<Form signUp error={errorProp}/>),
         confirmPasswordInput = queryAllByText('Re-enter Password')
 
         expect(confirmPasswordInput).toHaveLength(1)
     })
     it('Has a submit button', () => {
-        const { queryAllByText } = render(<Form/>),
+        const { queryAllByText } = render(<Form error={errorProp}/>),
         submitButtons = queryAllByText('Submit')
 
         expect(submitButtons).toHaveLength(1)
