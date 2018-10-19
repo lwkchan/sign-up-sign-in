759c8563c22c705f2550bad204a9f8e2ff90c855
lwkchan <laurachan205@gmail.com>
2018-10-19 20:54:24 +0100

    style: add help tooltips to forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Question } from 'styled-icons/octicons/Question'
+import Tippy from '@tippy.js/react'
+import 'tippy.js/dist/tippy.css'
 
 const StyledForm = styled.form`
 	display: flex;
 	flex-direction: column;`,
+Label = styled.label`
+	flex-grow: 2;
+`,
 LabelText = styled.span`
 	font-size: 20px;
 	font-weight: bold;
+`,
+Entry = styled.div`
+	display: flex
 `,
 Input = styled.input`
 	border: 1px solid #4195fc;
@@ -34,8 +43,11 @@ SubmitButton = styled.button`
 ErrorMessage = styled.p`
 	color: #FA5858;
 	text-align: center;
+`,
+QuestionIcon = styled(Question)`
+	height: 20px;
+	margin: auto 20px;
 `
-
 
 class Form extends Component {
 	constructor(props) {
@@ -76,28 +88,52 @@ class Form extends Component {
 				<StyledForm
 					className={`form ${this.getModifierClassName(this)}`}
 					onSubmit={this.props.handleSubmit}>
-					<label> <LabelText>Email</LabelText>
-						<Input
-						type="text"
-						name="email"
-						value={this.state.email}
-						onChange={this.handleInputChange} />
-					</label>
-					<label> <LabelText>Password</LabelText>
-						<Input
-						type="password"
-						name="password"
-						value={this.state.password}
-						onChange={this.handleInputChange} />
-					</label>
+					<Entry>
+						<Label> <LabelText>Email</LabelText>
+							<Input
+							type="text"
+							name="email"
+							value={this.state.email}
+							onChange={this.handleInputChange} />
+						</Label>
+						<Tippy
+							content="Please enter a valid email address which has not been used on this site before"
+							trigger="mouseenter focus"
+							placement="right">
+							<QuestionIcon/>
+						</Tippy>
+					</Entry>
+					<Entry>
+						<Label> <LabelText>Password</LabelText>
+							<Input
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handleInputChange} />
+						</Label>
+						<Tippy
+							content="Use a password with over eight characters and a mixture of letters and numbers."
+							trigger="mouseenter focus"
+							placement="right">
+							<QuestionIcon/>
+						</Tippy>
+					</Entry>
 					{this.props.signUp &&
-						<label> <LabelText>Re-enter Password</LabelText>
-						<Input
-						type="password"
-						name="confirmPassword"
-						value={this.state.confirmPassword}
-						onChange={this.handleInputChange} />
-						</label>
+						<Entry>
+							<Label> <LabelText>Re-enter Password</LabelText>
+							<Input
+							type="password"
+							name="confirmPassword"
+							value={this.state.confirmPassword}
+							onChange={this.handleInputChange} />
+							</Label>
+							<Tippy
+								content="Rewrite your password here to confirm it is correct"
+								trigger="mouseenter focus"
+								placement="right">
+								<QuestionIcon/>
+							</Tippy>
+						</Entry>
 					}
 					<SubmitButton error={this.props.error} name="Submit" type="submit" value="Submit">Submit</SubmitButton>
 				</StyledForm>
